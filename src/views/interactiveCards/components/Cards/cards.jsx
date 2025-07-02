2beb5bcdd4125bde014b0a5797cde5fb0e1d23a8
Christian Montoya <chrimonlo@gmail.com>
2025-07-02 16:22:14 -0500

    feat(chore): Adding states and props for the inputs

--- src/views/interactiveCards/components/Cards/cards.jsx
+++ src/views/interactiveCards/components/Cards/cards.jsx
@@ -1,13 +1,13 @@
 import { CardBack } from './CardBack/cardBack'
 import { CardFront } from './CardFront/cardFront'
 import styles from './cardsStyles.module.scss'
-function Cards() {
 
+function Cards({ cardName, cardNumber, expMonth, expYear, cvc }) {
   return (
     <section className={styles.cards}>
       <div className={styles.contentCards}>
-      <CardBack/>
-      <CardFront/>
+      <CardBack cvc={cvc}/>
+      <CardFront cardName={cardName} cardNumber={cardNumber} expMonth={expMonth} expYear={expYear}/>
       </div>
     </section>
   )
--- src/views/interactiveCards/components/interactiveCards.jsx
+++ src/views/interactiveCards/components/interactiveCards.jsx
@@ -1,12 +1,18 @@
 import styles from './interactiveCardsStyles.module.scss';
 import { Cards } from './Cards/cards';
 import { FormData } from './FormData/formData';
-function InteractiveCards() {
+import { useState } from 'react';
 
+function InteractiveCards() {
+  const [cardName, setCardName] = useState('JANE APPLESEED');
+  const [cardNumber, setCardNumber] = useState('0000 0000 0000 0000');
+  const [expMonth, setExpMonth] = useState('00');
+  const [expYear, setExpYear] = useState('00');
+  const [cvc, setCvc] = useState('000');
   return (
     <section className={styles.container}>
-      <Cards />
-      <FormData/>
+      <Cards cardName={cardName} cardNumber={cardNumber} expMonth={expMonth} expYear={expYear} cvc={cvc} />
+      <FormData cardName={cardName} setCardName={setCardName} cardNumber={cardNumber} setCardNumber={setCardNumber} expMonth={expMonth} setExpMonth={setExpMonth} expYear={expYear} setExpYear={setExpYear} cvc={cvc} setCvc={setCvc} />
     </section>
   )
 }
